ee6ed55b4b8f918dc5a2ab6c221f3f58619b8095
Davi Batista <davideosmar11@gmail.com>
2023-05-10 14:56:42 -0300

    Configurando pastas e componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,10 @@ const HomeNoAuth = ()   =>  {
     return (
         <>
             <Head>
-                
+                <title>OneBitFlix</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+                <meta property="og:title" content="Onebitflix" key="title"/>
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
             </Head>
             <main></main>
         </>
