d161867a708bbb47dcd7e1632060e8bb1eca3201
Davi Batista <davideosmar11@gmail.com>
2023-05-12 17:02:45 -0300

    eção de cursos em destaque

---
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.scss';
+import useSWR from 'swr';
+import courseService, { CourseType } from '@/services/courseService';
+import HeaderAuth from '@/components/common/headerAuth';
+import { Button, Container } from 'reactstrap';
+import Link from 'next/link';
+
+
+const FeaturedSection = function    ()  {
+    const {data, error} = useSWR('/featured', courseService.getFeaturedCourses)
+
+    if(error) return error
+    if(!data) return(<><p>Lorem</p></>)
+
+    return  (
+                <>
+                    {
+                        data.data?.map((course: CourseType)=>(
+                            <div key={course.id} style={{
+                                backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`, backgroundSize: "cover", backgroundPosition: "center", height: "480px"
+                            }}>
+                                <HeaderAuth/>
+                                <Container className='pt-4'>
+                                    {/* <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} /> */}
+                                    <p className={styles.title}>{course.name}</p>
+                                    <p className={styles.description}>{course.synopsis}</p>
+                                    <Link href={`/courses/${course.id}`} style={{
+                                        textDecoration: "none"
+                                    }}>
+                                        <Button outline color='light' className={styles.button}>
+                                            ASSISTIR
+                                            <img src="/playButton.png" alt="buttonImg" className={styles.buttonImg} />
+                                        </Button>
+                                    </Link>
+                                </Container>
+                            </div>
+                            ))[0]
+                        }
+                </>
+    )
+}
+
+export default FeaturedSection
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -27,6 +27,20 @@ const courseService =   {
 
         return response
     },
+    getFeaturedCourses: async () => {
+        const token = sessionStorage.getItem('onebitflix-token')
+
+        const response = await api.get('/courses/featured', {
+            headers:    {
+                Authorization: `Bearer ${token}`,
+            },
+        }).catch((error)    =>  {
+            console.log(error.response.data.message)
+
+            return error.response
+        })
+        return response
+    },
 }
 
 export default courseService
