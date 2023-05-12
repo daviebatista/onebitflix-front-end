e5a5a47e256224da41d775dcc5b1f19b891f1cd0
Davi Batista <davideosmar11@gmail.com>
2023-05-12 11:54:32 -0300

    Login interligado ao back-end

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,50 +1,118 @@
-import styles from '../styles/registerLogin.module.scss';
-import Head from 'next/head';
-import HeaderGeneric from '@/components/common/headerGeneric';
-import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import Footer from '@/components/common/footer';
+import styles from "../styles/registerLogin.module.scss";
+import Head from "next/head";
+import HeaderGeneric from "../src/components/common/headerGeneric";
+import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
+import Footer from "../src/components/common/footer";
+import { useRouter } from "next/router";
+import { FormEvent, useEffect, useState } from "react";
+import ToastComponent from "../src/components/common/toast";
+import authService from "../src/services/authService";
 
+const Login = function () {
+    const router = useRouter()
+    const [toastColor, setToastColor] = useState("")
+    const [toastIsOpen, setToastIsOpen] = useState(false)
+    const [toastMessage, setToastMessage] = useState("")
 
-const Login = function()    {
-    return  (
-        <>
-            <Head>
-                <title>Veritas - Login</title>
-                <link rel="icon" href="/favicon.png" type="image/x-icon" />
-                <meta name="description" content="Faça login para assistir nossas aulas!"/>
-            </Head>
-            <main className={styles.main}>
-                <HeaderGeneric logoUrl='/' btnUrl='/register' btnContent='Inscrever-me'/>
-                <Container className='py-5'>
-                    <p className={styles.formTitle}>
-                        Bem-vindo de volta!
-                    </p>
-                    <Form className={styles.form}>
-                        <p className='text-center'>
-                            <strong>Bem-vindo ao Veritas!</strong>
+    useEffect(() => {
+        if (sessionStorage.getItem("veritas-token")) {
+        router.push("/home")
+        }
+    }, [])
+
+    useEffect(() => {
+    const registerSucess = router.query.registred;
+
+    if (registerSucess === "true") {
+        setToastColor("bg-success")
+        setToastIsOpen(true)
+        setTimeout(() => {
+            setToastIsOpen(false)
+        }, 1000 * 3)
+        setToastMessage("Cadastro feito com sucesso!")
+    }
+}, [router.query])
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const email = formData.get("email")!.toString()
+    const password = formData.get("password")!.toString()
+    const params = { email, password }
+
+    const { status } = await authService.login(params)
+
+    if (status === 200) {
+        router.push("/home")
+    } else {
+        setToastColor("bg-danger")
+        setToastIsOpen(true)
+        setTimeout(() => {
+        setToastIsOpen(false)
+      }, 1000 * 3);
+        setToastMessage("Email ou senha incorretos!")
+    }
+};
+
+    return (
+            <>
+                <Head>
+                    <title>Veritas - Login</title>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+                </Head>
+                <main className={styles.main}>
+                    <HeaderGeneric
+                    logoUrl="/"
+                    btnUrl="/register"
+                    btnContent="Quero fazer parte"
+                    />
+                    <Container className="py-5">
+                    <p className={styles.formTitle}>Bem-vindo(a) de volta!</p>
+                    <Form className={styles.form} onSubmit={handleLogin}>
+                        <p className="text-center">
+                        <strong>Bem-vindo(a) ao Veritas!</strong>
                         </p>
                         <FormGroup>
-                            <Label for='email' className={styles.label}>
-                                E-mail
-                            </Label>
-                            <Input id='email' name='email' type='email' placeholder='Insira o seu e-mail' required className={styles.input} />
+                        <Label for="email" className={styles.label}>
+                            E-MAIL
+                        </Label>
+                        <Input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="Qual o seu email?"
+                            required
+                            className={styles.input}
+                        />
                         </FormGroup>
                         <FormGroup>
-                            <Label for='password' className={styles.label}>
-                                E-mail
-                            </Label>
-                            <Input id='password' name='password' type='password' placeholder='Insira a sua senha' required className={styles.input} />
+                        <Label for="password" className={styles.label}>
+                            SENHA
+                        </Label>
+                        <Input
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder="Qual a sua senha?"
+                            required
+                            className={styles.input}
+                        />
                         </FormGroup>
-                        <Button outline className={styles.formBtn}>
-                            LOGAR
+                        <Button type="submit" outline className={styles.formBtn}>
+                        ENTRAR
                         </Button>
                     </Form>
-                </Container>
-                <Footer/>
-            </main>
-
-        </>
-    )
+                    <ToastComponent
+                        color={toastColor}
+                        isOpen={toastIsOpen}
+                        message={toastMessage}
+                    />
+                    </Container>
+                    <Footer />
+                </main>
+            </>
+        )
 }
 
 export default Login
