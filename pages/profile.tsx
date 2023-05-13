633e2d79fe09895cb59686165fb13c5c6943b7fe
Davi Batista <davideosmar11@gmail.com>
2023-05-13 09:26:31 -0300

    Estrutura do profile

---
+++ pages/profile.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import styles from "../styles/profile.module.scss";
+import UserForm from "@/components/profile/user";
+import HeaderAuth from "@/components/common/headerAuth";
+import { Button, Col, Container, Row } from "reactstrap";
+import Footer from "@/components/common/footer";
+
+const UserInfo = function   ()  {
+    return(
+        <>
+            <Head>
+                <title>Meus dados</title>
+                <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+            </Head>
+            <main>
+                <div className={styles.header}>
+                    <HeaderAuth/>
+                </div>
+                <Container className="py-5">
+                    <p className={styles.title}>Minha conta</p>
+                    <Row className="pt-3 pb-5">
+                        <Col md={4} className={styles.btnColumn}>
+                            <Button className={styles.renderForm}>Dados Pessoais</Button>
+                            <Button className={styles.renderForm}>Senha</Button>
+                        </Col>
+                        <Col md>
+                            <UserForm/>
+                        </Col>
+                    </Row>
+                </Container>
+                <div className={styles.footer}>
+                    <Footer/>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default UserInfo
